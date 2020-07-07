461e8c99e8f82dae7cd650fe8afcf690803ad06c
santanarscs <raphaelstn@gmail.com>
2020-07-07 16:05:47 -0300

    add styled components

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import {AppProps } from 'next/app'
+import GlobalStyle from '../styles/global'
+
+const App = ({Component, pageProps}: AppProps) => (
+  <>
+    <Component {...pageProps} />
+    <GlobalStyle />
+  </>
+)
+
+export default App
