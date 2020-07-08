50737e2c7aefb595a6f75565914b2986a784f3fd
santanarscs <raphaelstn@gmail.com>
2020-07-08 11:26:39 -0300

    create index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,46 @@
+import { useCallback } from 'react'
+import Router from 'next/router'
+import Search from '../components/Search'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  h1 {
+    font-size: 26px;
+    line-height: 32px;
+    text-align: center;
+  }
+`
 export default function Home()  {
-  return <h1>Hello world</h1>
+
+  const handleSearchChannel = useCallback((search: string) => {
+    Router.push({
+      pathname: '/channel',
+      query: { search }
+    },
+    undefined,
+    { shallow: true }
+    )
+  }, [])
+
+  return(
+    <Container>
+      <Content>
+        <h1>Descubra as estatisticas dos seus canais preferidos do youtube</h1>
+        <Search 
+          placeholder="Buscar por canal"
+          handleSearch={handleSearchChannel}
+        />
+      </Content>
+    </Container>
+  )
 }
